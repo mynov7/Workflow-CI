14d7ae839585a23652458238aaa51a9199df1a56
Novia Br <noviabrsipakkar03@gmail.com>
2025-12-20 09:42:08 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import joblib
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+def train_model():
+    os.makedirs("artifacts", exist_ok=True)
+
+    df = pd.read_csv("loan_data_preprocessed.csv")
+    print("Daftar kolom yang ditemukan:", df.columns.tolist())
+
+    X = df.drop("loan_status", axis=1)
+    y = df["loan_status"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    mlflow.set_experiment("Loan_Experiment")
+
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+
+        print(f"Model Accuracy: {acc:.2f}")
+        print(classification_report(y_test, y_pred))
+
+        # Log ke MLflow
+        mlflow.log_param("model_type", "RandomForest")
+        mlflow.log_metric("accuracy", acc)
+
+        mlflow.sklearn.log_model(model, "model_loan_artifact")
+
+        # Simpan model
+        model_path = "artifacts/model_loan.pkl"
+        joblib.dump(model, model_path)
+        print(f"Model berhasil disimpan di {model_path}")
+
+if __name__ == "__main__":
+    train_model()
